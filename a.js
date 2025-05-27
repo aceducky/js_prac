287d677dfb01aaccac46bfee37b29508c0fc1fc5
SrinivasD <115702816+aceducky@users.noreply.github.com>
2025-05-27 18:07:17 +0530

    new file:   .gitignore 	new file:   a.js 	new file:   prac.ipynb

---
+++ a.js
@@ -0,0 +1,29 @@
+// const o1 = {
+//   id: 1,
+//   sayId: function () {
+//     console.log(this.id);
+//   },
+// };
+
+// o1.sayId();
+// o1["sayId"]();
+
+// const o2 = {
+//   id: 1,
+//   sayId: () => {
+//     console.log(this.id);
+//   },
+// };
+
+// o2.sayId();
+
+// // function sayId() {
+// //     console.log("ID func");
+// // }
+// // sayId();
+
+// function f1() {
+//   this.id = "f1's ID";p
+// }
+// console.log(new f1().id);
+
